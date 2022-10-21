ca286f0b52debaa52817bebe2fbb92cb67c6bf62
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-21 16:55:45 +0600

    colors config added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 
 const Layout = () => {
   return (
-    <div className='flex text-white h-screen'>
-      <div className='bg-gray-800 p-3 space-y-2 overflow-y-scroll'>
+    <div className='flex text-gray-100 h-screen'>
+      <div className='bg-gray-900 p-3 space-y-2 overflow-y-scroll'>
         {[...Array(40)].map((_, i) => (
           <div className='bg-white text-gray-800 w-12 h-12 rounded-full flex justify-center items-center'>
             {i}
           </div>
         ))}
       </div>
-      <div className='bg-gray-700 w-60 flex flex-col'>
+      <div className='bg-gray-800 w-60 flex flex-col'>
         <div className='px-3 h-12 flex items-center shadow-md flex-shrink-0'>
           Tailwind CSS
         </div>
-        <div className='p-3 flex-1 overflow-y-scroll'>
+        <div className='text-gray-300 p-3 flex-1 overflow-y-scroll'>
+          <p className='text-white'>Channel (unread)</p>
           {[...Array(40)].map((_, i) => (
             <p>Channel: {i}</p>
           ))}
         </div>
       </div>
-      <div className='bg-gray-600 flex-1 flex flex-col'>
+      <div className='bg-gray-700 flex-1 flex flex-col'>
         <div className='px-3 h-12 flex items-center shadow-md'>general</div>
         <div className='p-3 flex-1 overflow-y-scroll space-y-4'>
           {[...Array(40)].map((_, i) => (
