7d4a5d6a75e518fe4e1bfe4d95dc62d811dfc702
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-29 23:58:21 +0600

    shadow opacity fixed

--- src/components/Server.jsx
+++ src/components/Server.jsx
@@ -7,7 +7,7 @@ const Server = () => {
   return (
     <>
       <div className='bg-gray-800 w-60 flex flex-col'>
-        <div className='px-3 h-12 flex items-center shadow-md flex-shrink-0'>
+        <div className='px-3 h-12 flex items-center shadow-sm flex-shrink-0'>
           <p>Server: {id}</p>
         </div>
         <div className='text-gray-300 p-3 flex-1 overflow-y-scroll'>
@@ -17,7 +17,7 @@ const Server = () => {
         </div>
       </div>
       <div className='bg-gray-700 flex-1 flex flex-col'>
-        <div className='px-3 h-12 flex items-center shadow-md'>general</div>
+        <div className='px-3 h-12 flex items-center shadow-sm'>general</div>
         <div className='p-3 flex-1 overflow-y-scroll space-y-4'>
           {[...Array(parseInt(id) + 1)].map((_, i) => (
             <SingleMessage key={i} i={i} />
